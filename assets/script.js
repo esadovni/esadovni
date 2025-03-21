d1e775d50e0d8d8d796325b9e507dff37d672ec8
esadovni <139903117+esadovni@users.noreply.github.com>
2025-03-21 00:55:54 -0700

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const themeToggle = document.getElementById("theme-toggle");
+    themeToggle.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+    });
+});
